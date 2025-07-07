3ce7145df444ccc2ceb08272b073605ecb8f370a
Carlos Vega <carlosevega03@gmail.com>
2025-07-07 16:44:10 -0400

    Reorganize ENV and load agents

--- g1_rl.py
+++ g1_rl.py
@@ -1,8 +1,10 @@
+import copy
 from typing import Any, Dict, Union
 
 import numpy as np
 import sapien
 import torch
+import os
 
 from mani_skill.agents.robots.unitree_g1.g1 import UnitreeG1
 
@@ -11,21 +13,26 @@ from mani_skill.sensors.camera import CameraConfig
 from mani_skill.utils import common, sapien_utils
 from mani_skill.utils.registration import register_env
 from mani_skill.utils.structs.types import SimConfig
+from mani_skill.utils.scene_builder.kitchen_counter import KitchenCounterSceneBuilder
+from mani_skill.utils.structs.types import GPUMemoryConfig, SceneConfig
 
 
-@register_env("G1RL-v1", max_episode_steps=200)
-class CustomEnv(BaseEnv):
-
-    SUPPORTED_ROBOTS = ["UnitreeG1"]
-    agent: UnitreeG1
+class HumanoidPickPlaceEnv(BaseEnv):
+    SUPPORTED_REWARD_MODES = ["sparse", "none"]
+    """sets up a basic scene with an item to pick up and a fridge to place it in"""
+    kitchen_scene_scale = 1.0
 
     def __init__(self, *args, robot_init_qpos_noise=0.02, **kwargs):
         self.robot_init_qpos_noise = robot_init_qpos_noise
-        super().__init__(*args, robot_uids="unitree_g1", **kwargs)
+        super().__init__(*args, **kwargs)
 
     @property
     def _default_sim_config(self):
-        return SimConfig()
+        return SimConfig(
+            gpu_memory_config=GPUMemoryConfig(
+                max_rigid_contact_count=2**22,
+            )
+        )
 
     @property
     def _default_sensor_configs(self):
@@ -40,12 +47,97 @@ class CustomEnv(BaseEnv):
         return CameraConfig("render_camera", pose=pose, width=512, height=512, fov=1)
 
     def _load_agent(self, options: dict):
-        super()._load_agent(options, sapien.Pose(p=[0, 0, 0]))
+        super()._load_agent(options, sapien.Pose(p=[0, 0, 1]))
 
     def _load_scene(self, options: dict):
-        pass
+        self.scene_builder = KitchenCounterSceneBuilder(self)
+        self.kitchen_scene = self.scene_builder.build(scale=self.kitchen_scene_scale)
+
+        # Refrigerator
+        scene = sapien.Scene()
+        scene.set_timestep(1 / 100.0)
+        loader = scene.create_urdf_loader()
+        loader.fix_root_link = True
+        current_file_path = os.path.abspath(__file__)
+        current_directory = os.path.dirname(current_file_path)
+        urdf_path = "/assets/11211/mobility.urdf"
+        self.fridge = loader.load(current_directory + urdf_path)
+        assert self.fridge, "failed to load URDF."
+
 
     def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
+        self.scene_builder.initialize(env_idx)
+
+    def evaluate(self):
+        return {
+            "success": torch.zeros(self.num_envs, device=self.device, dtype=bool),
+            "fail": torch.zeros(self.num_envs, device=self.device, dtype=bool),
+        }
+
+    def _get_obs_extra(self, info: Dict):
+        return dict()
+
+
+
+@register_env("G1RL-v1", max_episode_steps=200)
+class UnitreeG1PutInFridge(HumanoidPickPlaceEnv):
+    """
+    **Task Description:**
+    Control the humanoid unitree G1 robot to grab an item with its right arm and place it inside a fridge
+
+    **Randomizations:**
+    - the items's xy position is randomized on top of a table in the region [0.025, 0.025] x [-0.025, -0.025]. It is placed flat on the table
+    - the item's z-axis rotation is randomized to a random angle
+
+    **Success Conditions:**
+    - the item position is within the bounds of the fridge's shelf.
+    - the fridge door is closed
+
+    **Goal Specification:**
+    - The fridges's 3D position
+    """ 
+    SUPPORTED_ROBOTS = ["UnitreeG1"]
+    SUPPORTED_REWARD_MODES = ["normalized_dense", "dense", "sparse", "none"]
+    agent: Union[UnitreeG1]
+
+    def __init__(self, *args, robot_uids="unitree_g1", **kwargs):
+        self.init_robot_pose = copy.deepcopy(
+            UnitreeG1.keyframes["standing"].pose
+        )
+        self.init_robot_pose.p = [-0.3, 0, 0.755]
+        super().__init__(*args, robot_uids=robot_uids, **kwargs)
+
+    @property
+    def _default_sim_config(self):
+        return SimConfig(
+            gpu_memory_config=GPUMemoryConfig(
+                max_rigid_contact_count=2**22, max_rigid_patch_count=2**21
+            ),
+            # TODO (stao): G1 robot may need some custom collision disabling as the dextrous fingers may often be close to each other
+            # and slow down simulation. A temporary fix is to reduce contact_offset value down so that we don't check so many possible
+            # collisions
+            scene_config=SceneConfig(contact_offset=0.01),
+        )
+
+    @property
+    def _default_sensor_configs(self):
+        pose = sapien.Pose([0.279123, 0.303438, 1.34794], [0.252428, 0.396735, 0.114442, -0.875091])
+        return CameraConfig(
+            uid="base_camera", pose=pose, width=128, height=128, fov=np.pi / 2, near=0.01, far=100,
+        )
+
+    @property
+    def _default_human_render_camera_configs(self):
+        pose = sapien.Pose([0.279123, 0.303438, 1.34794], [0.252428, 0.396735, 0.114442, -0.875091])
+        return CameraConfig(
+            uid="render_camera", pose=pose, width=512, height=512, fov=np.pi / 2, near=0.01, far=100,
+        )
+
+    def _load_scene(self, options: Dict):
+        super()._load_scene(options)
+
+
+    def _initialize_episode(self, env_idx: torch.Tensor, options: Dict):
         pass
 
     def evaluate(self):
@@ -65,3 +157,4 @@ class CustomEnv(BaseEnv):
     ):
         max_reward = 1.0
         return self.compute_dense_reward(obs=obs, action=action, info=info) / max_reward
+    
---
+++ test.py
@@ -0,0 +1,24 @@
+import gymnasium as gym
+import mani_skill.envs
+import g1_rl
+import ppo
+
+env = gym.make(
+    "G1RL-v1",
+    #"UnitreeG1PlaceAppleInBowl-v1",
+    num_envs=1,
+    obs_mode="state",
+    control_mode="pd_joint_delta_pos",
+    render_mode="human"
+)
+print("Observation Space", env.observation_space)
+print("Action Space", env.action_space)
+
+obs, _ = env.reset(seed=0)
+done = False
+while True:
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+    done = terminated or truncated
+    env.render()
+
